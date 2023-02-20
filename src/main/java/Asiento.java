977f352b3c71cd1b130efbb2ae6df46921bdc35a
jlotero23 <125835077+jlotero23@users.noreply.github.com>
2023-02-19 19:41:17 -0500

    Asiento

---
+++ src/main/java/Asiento.java
@@ -0,0 +1,17 @@
+public class Asiento {
+	String color;
+	int precio;
+	int registro;
+	String[] colores = {"negro","blanco","rojo","verde","amarillo"};
+	void CambiarColor(String color) {
+		color=color.toLowerCase();
+		for (int i=0;i<colores.length;i++) {
+			if(color.equals(colores[i])) {
+				this.color = color;
+				break;
+			}
+		}
+		
+	}
+}
+
